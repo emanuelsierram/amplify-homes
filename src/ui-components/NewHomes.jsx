50ef3ba28722b6bf1607e41cfe577c9a06c5ac1c
Emanuel Ricardo Sierra Muentes - Ceiba Software <emanuel.sierra@ceiba.com.co>
2021-12-16 17:06:59 -0500

    Agregando footer y navbar

---
+++ src/ui-components/NewHomes.jsx
@@ -0,0 +1,48 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import React from "react";
+import {
+  getOverrideProps,
+  useDataStoreBinding,
+} from "@aws-amplify/ui-react/internal";
+import CardA from "./CardA";
+import { Collection } from "@aws-amplify/ui-react";
+export default function NewHomes(props) {
+  const { Home, items: itemsProp, overrides: overridesProp, ...rest } = props;
+  const overrides = { ...overridesProp };
+  const items =
+    itemsProp !== undefined
+      ? itemsProp
+      : useDataStoreBinding({
+          type: "collection",
+          model: Home,
+        }).items;
+  return (
+    <Collection
+      templateColumns="1fr 1fr 1fr"
+      type="grid"
+      alignItems="stretch"
+      justifyContent="stretch"
+      autoFlow="row"
+      items={items || []}
+      {...rest}
+      {...getOverrideProps(overrides, "Collection")}
+    >
+      {(item, index) => (
+        <CardA
+          width="auto"
+          margin="10px 10px 10px 10px"
+          Home={item}
+          height="auto"
+          key={item.id}
+          {...getOverrideProps(overrides, "Collection.CardA[0]")}
+        ></CardA>
+      )}
+    </Collection>
+  );
+}
